625389e3ec8e7d41d000d42079bb211aa74cc165
thsghdud13 <thsgdud25@naver.com>
2024-03-03 15:51:36 +0900

    Chore : project settings

---
+++ src/main/java/coin/auto_trading/AutoTradingApplication.java
@@ -0,0 +1,13 @@
+package coin.auto_trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coin/auto_trading/AutoTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package coin.auto_trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
